9440828feb112bcb4a20808bfa2fa4d5e51b83d7
adambb9 <55655523+adambb9@users.noreply.github.com>
2021-09-08 13:10:42 +0200

    snake body piece added

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
 #building the classic snake game
+from turtle import Screen, Turtle
+
+screen  = Screen ()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+
+snake_body = []
+
+x_pos = 0
+
+length_of_snake = 3
+
+for i in range(length_of_snake):
+    new_snake = Turtle("square")
+    new_snake.penup()
+    new_snake.pencolor("white")
+    new_snake.fillcolor("white")
+    snake_body.append(new_snake)
+    new_snake.setpos(x_pos - (snake_body.index(new_snake)*20) - (snake_body.index(new_snake)*1), 0)
+
+
+
+
+
+
+screen.exitonclick()
