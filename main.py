81f736f047efc0a81c868af426cce280c57368f5
adambb9 <55655523+adambb9@users.noreply.github.com>
2021-09-08 13:38:10 +0200

    main game loop added

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 #building the classic snake game
 from turtle import Screen, Turtle
+import time
 
 screen  = Screen ()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
+screen.tracer(0)
 
 snake_body = []
+
+
 
 x_pos = 0
 
@@ -18,8 +22,23 @@ for i in range(length_of_snake):
     new_snake.pencolor("white")
     new_snake.fillcolor("white")
     snake_body.append(new_snake)
-    new_snake.setpos(x_pos - (snake_body.index(new_snake)*20) - (snake_body.index(new_snake)*1), 0)
+    new_snake.setpos((x_pos - (snake_body.index(new_snake)*20)), 0)
 
+
+
+game_is_on = True
+
+while game_is_on:
+    screen.update()
+    time.sleep(1)
+
+    for segment in range(len(snake_body) - 1, 0, -1):
+        new_x = snake_body[segment -1].xcor()
+        new_y = snake_body[segment -1].ycor()
+        snake_body[segment].goto(new_x, new_y)
+    snake_body[0].forward(20)
+
+        
 
 
 
