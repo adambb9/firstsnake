e854eae963cf4db48e56d84021228d84b31e5401
adambb9 <55655523+adambb9@users.noreply.github.com>
2021-09-09 21:20:19 +0200

    scoreboard fixed

--- scoreboard.py
+++ scoreboard.py
@@ -2,8 +2,8 @@ from turtle import Turtle
 
 class Scoreboard(Turtle):
 
-    def __init(self):
-        super().init()
+    def __init__(self):
+        super().__init__()
         self.color("white")
         self.write("Score: ", align="center")
         self.score = 0
