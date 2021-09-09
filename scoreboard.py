b9a8f907be3faeed1fbb2f55c9925fe7e8998119
adambb9 <55655523+adambb9@users.noreply.github.com>
2021-09-09 21:23:38 +0200

    scoreboard shows on screen

--- scoreboard.py
+++ scoreboard.py
@@ -4,6 +4,10 @@ class Scoreboard(Turtle):
 
     def __init__(self):
         super().__init__()
+        self.hideturtle()
         self.color("white")
-        self.write("Score: ", align="center")
         self.score = 0
+        self.write(F"Score: {self.score}", align="center")
+
+    #score board writes to screen but doesnt update
+        
